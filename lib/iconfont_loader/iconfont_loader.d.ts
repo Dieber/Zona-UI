cfe1f7d9a0f96d241316613d76ee5cb62975bdfd
Dieber <a451120068@gmail.com>
2019-07-12 17:01:02 +0800

    add some feature

--- lib/iconfont_loader/iconfont_loader.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface IconfontLoaderProps {
-    iconUrl: string;
-}
-interface IIconfontLoader extends React.FunctionComponent<IconfontLoaderProps> {
-    isIconFontUrl(iconUrl: string): boolean;
-}
-declare const IconfontLoader: IIconfontLoader;
-export default IconfontLoader;
