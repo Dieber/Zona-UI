3677f5000d801144f6fd4b8ce8cc7b71acbea75b
Dieber <a451120068@gmail.com>
2019-09-02 18:21:06 +0800

    routine update

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.styl';
 import Icon from './icon/icon'
-// import 'http://at.alicdn.com/t/font_1277302_3478stw4cy9.js'
 
 ReactDOM.render(<Icon name="EURO"/>,document.querySelector('#root'))
+
