2258c81642157ce54ff5f56100bf023006879e97
Dieber <451120068@qq.com>
2019-09-03 13:12:09 +0800

    [fix] index.html fixed

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,19 +2,24 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const base = require('./webpack.config')
 module.exports = Object.assign({}, base, {
-    mode: "production",
-    externals: {
-      react: {
-        commonjs: 'react',
-        commonjs2: 'react',
-        amd: 'react',
-        root: 'React'
-      },
-      'react-dom': {
-        commonjs: 'react-dom',
-        commonjs2: 'react-dom',
-        amd: 'react-dom',
-        root: 'ReactDOM'
-      },
-    }
+  mode: "production",
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM'
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
 })
